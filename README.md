52a2c88b45db9eec7cc671b6f279e6d35eaa7402
BunnY <w@bunnywong.com>
2016-01-31 23:50:27 +0800

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+http://blog.tradingview.com/?p=1633
+https://www.tradingview.com/stock-charts-support/index.php/Directional_Movement_(DMI)#CALCULATION
+
+https://www.tradingview.com/study-script-reference/#fun_alertcondition
+
---
+++ dmi.js
@@ -0,0 +1,13 @@
+study(title="Directional Movement Index", shorttitle="DMI")
+len = input(14, minval=1, title="DI Length")
+lensig = input(14, title="ADX Smoothing", minval=1, maxval=50)
+up = change(high)
+down = -change(low)
+trur = rma(tr, len)
+plus = fixnan(100 * rma(up > down and up > 0 ? up : 0, len) / trur)
+minus = fixnan(100 * rma(down > up and down > 0 ? down : 0, len) / trur)
+sum = plus + minus
+adx = 100 * rma(abs(plus - minus) / (sum == 0 ? 1 : sum), lensig)
+plot(plus, color=blue, title="+DI")
+plot(minus, color=orange, title="-DI")
+plot(adx, color=red, title="ADX")
