88579566c34341fce444ae1fe547acbdfbc0065a
BunnY Wong <bunny.wong@scmp.com>
2016-02-02 13:58:59 +0800

    function demo

--- README.md
+++ README.md
@@ -9,6 +9,11 @@ Multi chart
 https://www.tradingview.com/chart/OjvpeZh5/
 
 High signal
-  (8.8)-(4.2)<(1.8)-(4.2)<(1.8)-(12.12)
+  (8.8)-(4.2) < (1.8)-(4.2) < (1.8)-(12.12)
+  (+DI)-(ADX) < (ADX)-(ADX) < (ADX)-(-DI)
+  e.g.
+
 Low signal
-  (12.12)-(1.8)<(4.2)-(1.8)<(4.2)-(8.8)
+  (12.12)-(1.8) < (4.2)-(1.8) < (4.2)-(8.8)
+  (-DI)-(ADX) < (ADX)-(ADX) < (ADX)-(+DI)
+
--- dmi.js
+++ dmi.js
@@ -1,6 +1,7 @@
-study(title="Directional Movement Index", shorttitle="DMI")
-len = input(14, minval=1, title="DI Length")
-lensig = input(14, title="ADX Smoothing", minval=1, maxval=50)
+study(title="My - Directional Movement Index", shorttitle="DMI")
+
+len = input(8, minval=1, title="DI Length")
+lensig = input(12, title="ADX Smoothing", minval=1, maxval=50)
 up = change(high)
 down = -change(low)
 trur = rma(tr, len)
@@ -8,22 +9,22 @@ plus = fixnan(100 * rma(up > down and up > 0 ? up : 0, len) / trur)
 minus = fixnan(100 * rma(down > up and down > 0 ? down : 0, len) / trur)
 sum = plus + minus
 adx = 100 * rma(abs(plus - minus) / (sum == 0 ? 1 : sum), lensig)
-plot(plus, color=blue, title="+DI")
-plot(minus, color=orange, title="-DI")
-plot(adx, color=red, title="ADX")
+
+// plot(plus, color=blue, title="+DI")
+// plot(minus, color=orange, title="-DI")
+// plot(adx, color=red, title="ADX")
+
+hehe(a, b, method) =>
+    // isEqual = s == s[1]
+    ud = (a)
+    ud
+
+plot(hehe(8, 8, plus), color=blue, title="My Trial")
+
 
 // alertcondition(true, title='12,12 -DI', message='this is alert of...-DI(min)')
 
-/*
-  ADX / adx
-  +DI / plus
-  -DI / minus
+// High signal
+//   (8.8)-(4.2) < (1.8)-(4.2) < (1.8)-(12.12)
+//   (+DI)-(ADX) < (ADX)-(ADX) < (ADX)-(-DI)
 
-  High signal
-    (8.8)-(4.2)<(1.8)-(4.2)<(1.8)-(12.12)
-  Low signal
-    (12.12)-(1.8)<(4.2)-(1.8)<(4.2)-(8.8)
-
-  Existing ref:
-    12,12 -DI(minus)
-*/
