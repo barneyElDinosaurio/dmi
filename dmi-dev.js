d997c490e24e07d26c24dea29a9067e96a185a62
BunnY <w@bunnywong.com>
2016-02-15 02:44:52 +0800

    plot hight signal ABC

--- dmi-dev.js
+++ dmi-dev.js
@@ -1,19 +1,26 @@
-study(title="My - Directional Movement Index", shorttitle="DMI")
+study(title="My - Directional Movement Index", shorttitle="DMI hight signal")
 
 // ================================================================================
 // Default
-a1    = input(8, minval = 1, title  = "1. +DI")
-a2 = input(8, title  = "2. +DI")
+a1 = input(8, title="a1. +DI")
+a2 = input(8, title="a2. +DI")
+a3 = input(4, title="a3. ADX")
+a4 = input(2, title="a4. ADX")
 
+b1 = input(1, title="b1. ADX")
+b2 = input(8, title="b2. ADX")
+b3 = input(4, title="b3. ADX")
+b4 = input(2, title="b4. ADX")
+
+c1 = input(1, title="c1. ADX")
+c2 = input(8, title="c2. ADX")
+c3 = input(12, title="c3. -DI")
+c4 = input(12, title="c4. -DI")
+
+// ================================================================================
+// Declear variable
 up   = change(high)
 down = -change(low)
-
-trur = rma(tr, a1)
-// -Plus
-plus = fixnan(100 * rma(up > down and up > 0 ? up : 0, a1) / trur)
-
-// - Min
-minus = fixnan(100 * rma(down > up and down > 0 ? down : 0, a1) / trur)
 
 // ================================================================================
 // Function
@@ -29,28 +36,23 @@ myadx(a1, a2) =>
     answer = 100 * rma(abs(myplus(a1) - mymins(a1)) / (sum == 0 ? 1 : sum), a2)
 
 // ================================================================================
-// Default
-len2 = input(4, minval=1, title="3. ADX")
-lensig2 = input(2, title="4. ADX")
-
-up2 = change(high)
-down2 = -change(low)
-trur2 = rma(tr, len2)
-
-// -Plus
-plus2 = fixnan(100 * rma(up2 > down2 and up2 > 0 ? up : 0, len2) / trur2)
-
-// - Min
-minus2 = fixnan(100 * rma(down2 > up2 and down2 > 0 ? down2 : 0, len2) / trur2)
-
-// - ADX
-sum2 = plus2 + minus2
-adx2 = 100 * rma(abs(plus - minus2) / (sum2 == 0 ? 1 : sum2), lensig2)
-
 // Result
-plot(myplus(a1), color=blue, linewidth=1, title="plot +DI")
-// plot(adx, color=red, linewidth=1, title="plot ADX")
-// plot(myplus() - adx, color=green, linewidth=3, title="Hight signal - Plot: (+DI)-(ADX)")
+
+// A
+// plot(myplus(a1), color=orange, linewidth=1, title="+DI")
+// plot(myadx(a3, a4), color=orange, linewidth=1, title="ADX")
+plot(myplus(a1) - myadx(a3, a4), color=orange, linewidth=3, title="Hight signal - A: (+DI)-(ADX)")
+// plot(abs(myplus(a1) - myadx(a3, a4)), color=blue, linewidth=3, title="[ABS]Hight signal - A: (+DI)-(ADX)")
+
+// B
+// plot(myadx(b1, b2), color=green, linewidth=1, title="ADX")
+// plot(myadx(b3, b4), color=green, linewidth=1, title="ADX")
+plot(myadx(b1, b2) - myadx(b3, b4), color=green, linewidth=3, title="Hight signal - B: (ADX)-(ADX)")
+
+// C
+// plot(myadx(c1, c2), color=red, linewidth=1, title="ADX")
+// plot(mymins(c3), color=red, linewidth=1, title="-DI")
+plot(myadx(c1, c2) - mymins(c3), color=black, linewidth=3, title="Hight signal - C: (ADX)-(-DI)")
 
 // ================================================================================
 // High signal
