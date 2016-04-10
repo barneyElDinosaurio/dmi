43315a53e70024616449a3527264b5d6de161a68
BunnY <w@bunnywong.com>
2016-04-11 00:16:51 +0800

    add v1 from production

---
+++ hight-signal-v1.js
@@ -0,0 +1,60 @@
+study(title="My High - Directional Movement Index", shorttitle="High signal DMI")
+
+// ================================================================================
+// Default
+d1 = input(8, title="d1. +DI")
+d2 = input(8, title="d2. +DI")
+d3 = input(4, title="d3. ADX")
+d4 = input(2, title="d4. ADX")
+
+e1 = input(1, title="e1. ADX")
+e2 = input(8, title="e2. ADX")
+e3 = input(4, title="e3. ADX")
+e4 = input(2, title="e4. ADX")
+
+f1 = input(1, title="f1. ADX")
+f2 = input(8, title="f2. ADX")
+f3 = input(12, title="f3. -DI")
+f4 = input(12, title="f4. -DI")
+
+// ================================================================================
+// Declear variable
+up   = change(high)
+down = -change(low)
+
+// ================================================================================
+// Function
+myplus(d1) =>
+    trur = rma(tr, d1)
+    answer = fixnan(100 * rma(up > down and up > 0 ? up : 0, d1) / trur)
+
+mymins(d1) =>
+    trur = rma(tr, d1)
+    answer = fixnan(100 * rma(down > up and down > 0 ? down : 0, d1) / trur)
+
+mysum(d2) =>
+    answer = myplus(d2) + mymins(d2)
+
+myadx(d1, d2) =>
+    adx = 100 * rma(abs(myplus(d2) - mymins(d2)) / (mysum(d2) == 0 ? 1 : mysum(d2)), d1)
+
+// High signal
+// (8.8)-(4.2) < (1.8)-(4.2) < (1.8)-(12.12)
+// (+DI)-(ADX) < (ADX)-(ADX) < (ADX)-(-DI)
+
+// D. (ABS)
+d = abs(myplus(d1) - myadx(d3, d4))
+plot(d, color=blue, linewidth=1, title="A: (8.8)-(4.2) | (+DI)-(ADX)")
+
+// E.
+e = myadx(e1, e2) - myadx(e3, e4)
+plot(e, color=blue, linewidth=2, title="B: (1.8)-(4.2) | (ADX)-(ADX)")
+
+// F.
+f = myadx(f1, f2) - mymins(f3)
+plot(f, color=blue, linewidth=3, title="C: (1.8)-(12.12) | (ADX)-(-DI)")
+
+// Alert
+myalert = d < e < f ? 1 : 0
+mycolor = d < e < f ? gray : red
+// plot(myalert, color=mycolor, linewidth=5, title="Low alert")
---
+++ low-signal-v1.js
@@ -0,0 +1,59 @@
+study(title="My Low - Directional Movement Index", shorttitle="Low Signal DMI")
+
+// ================================================================================
+// Default
+a1 = input(12, title="a1. -DI")
+a2 = input(12, title="a2. -DI")
+a3 = input(1, title="a3. ADX")
+a4 = input(8, title="a4. ADX")
+
+b1 = input(4, title="b1. ADX")
+b2 = input(2, title="b2. ADX")
+b3 = input(1, title="b3. ADX")
+b4 = input(8, title="b4. ADX")
+
+c1 = input(4, title="c1. ADX")
+c2 = input(2, title="c2. ADX")
+c3 = input(8, title="c3. +DI")
+c4 = input(8, title="c4. +DI")
+
+// ================================================================================
+// Declear variable
+up   = change(high)
+down = -change(low)
+
+// ================================================================================
+// Function
+myplus(a1) =>
+    trur = rma(tr, a1)
+    answer = fixnan(100 * rma(up > down and up > 0 ? up : 0, a1) / trur)
+
+mymins(a1) =>
+    trur = rma(tr, a1)
+    answer = fixnan(100 * rma(down > up and down > 0 ? down : 0, a1) / trur)
+
+mysum(a2) =>
+    answer = myplus(a2) + mymins(a2)
+
+myadx(a1, a2) =>
+    adx = 100 * rma(abs(myplus(a2) - mymins(a2)) / (mysum(a2) == 0 ? 1 : mysum(a2)), a1)
+
+// Low signal
+// (12.12)-(1.8) < (4.2)-(1.8) < (4.2)-(8.8)
+// (-DI)-(ADX)   < (ADX)-(ADX) < (ADX)-(+DI)
+
+// A.
+a = abs(mymins(a1) - myadx(a4, a3))
+plot(a, color=orange, linewidth=3, title="A: (12.12)-(1.8) | (-DI)-(ADX)")
+
+// B.
+b = myadx(b1, b2) - myadx(b3, b4)
+plot(b, color=orange, linewidth=2, title="B: (4.2)-(1.8) | (ADX)-(ADX)")
+
+// C.
+c = myadx(c1, c2) - myplus(c3)
+plot(c, color=orange, linewidth=3, title="C: (4.2)-(8.8) | (ADX)-(+DI)")
+
+myalert = a < b < c ? 1 : 0
+mycolor = a < b < c ? gray : red
+plot(myalert, color=mycolor, linewidth=5, title="Low alert")
